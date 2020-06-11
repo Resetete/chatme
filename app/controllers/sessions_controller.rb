d14edb9b32a2ba8f12e294ebe02fd2398de4e5a7
Theresa <tmannschatz@gmail.com>
2020-06-11 13:45:48 +0200

    Add root and login routes, controllers, views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+
+  def new # login form and submit data
+  end
+
+  def create # handle new form login data
+  end
+
+  def destroy # logout - delete session
+  end
+
+end
