382c7c97e978a550bf554e65d5999efb9a4e5d75
Theresa <tmannschatz@gmail.com>
2020-06-16 13:53:57 +0200

    Allow login with login form, add helper methods to check the user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create # handle new form login data
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'You have successfully logged in'
+      redirect_to root_path
+    else
+      flash.now[:error] = "Credentials were wrong."
+      render 'new'
+    end
   end
 
   def destroy # logout - delete session
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
   end
 
 end
